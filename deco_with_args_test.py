43a83082ec9fe1b20a20ccc7f88c96c6d6cade28
zhenglong <zhenglong@pset.suntec.net>
2016-02-20 20:37:43 +0800

    decorator demos

---
+++ deco_with_args_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def decor(start):
+    def deco(*args, **kwargs):
+        print "before decorator!"
+        start(*args, **kwargs)
+        print "after decorator!\n"
+    return deco
+    
+
+@decor()
+def runFun(*args, **kwargs):
+    print "This is run method!, args = %s, kwargs = %s" % (args, kwargs)
+
+if __name__ == "__main__":
+    runFun(1, 2, x=3, y=4)
+    runFun(3, 4, m=5, n=6)
