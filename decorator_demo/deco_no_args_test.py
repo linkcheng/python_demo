39df22ef137db3f594b0551f265693a3bc8be5ea
linkcheng <zhenglong1992@126.com>
2016-02-21 23:42:59 +0800

    Update comment

--- decorator_demo/deco_no_args_test.py
+++ decorator_demo/deco_no_args_test.py
@@ -13,5 +13,5 @@ def runFun(*args, **kwargs):
     print "This is run method!, args = %s, kwargs = %s" % (args, kwargs)
 
 if __name__ == "__main__":
-    runFun(1, 2)
+    runFun(1, 2) # runFun(1, 2) = decor(runFun)(1, 2) => deco(1, 2)
     runFun(3, 4, m=5, n=6)
--- decorator_demo/deco_with_args_test.py
+++ decorator_demo/deco_with_args_test.py
@@ -15,5 +15,5 @@ def runFun(*args, **kwargs):
     print "This is run method!, args = %s, kwargs = %s" % (args, kwargs)
 
 if __name__ == "__main__":
-    runFun(1, 2)   # decor("mymodule")(runFun)
+    runFun(1, 2)   # runFun(1, 2) = decor("mymodule")(runFun)(1, 2) => _deco(runFun)(1, 2) => deco(1, 2)
     runFun(3, 4, m=5, n=6)
