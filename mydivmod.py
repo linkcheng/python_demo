6d1a8d82b885da2d5d2c70659a0dee014a95e9fc
zhenglong <zhenglong@pset.suntec.net>
2016-09-05 21:59:59 +0800

    update new way

--- mydivmod.py
+++ mydivmod.py
@@ -7,28 +7,38 @@
 # 代码中不得使用"/", "//", "%"等运算符及其它模块的帮助
 
 def mydivmod(dividend, divisor):
+    '''Funtion is the same as divmod'''
+
     if 0 == divisor:
-        return 'inf', 0
+        return 'INF', 0
 
     if 0 == dividend:
         return 0, 0
 
+    # By dividend and divisor, set step; if all are greater than zero, step is one, or is minus one.
     if dividend * divisor > 0:
-        quotient = 1
-        while False if abs(quotient) * abs(divisor) > abs(dividend) else True:
-            quotient += 1
-        quotient -= 1
+        step = 1
     else:
-        quotient = -1
-        while False if abs(quotient) * abs(divisor) > abs(dividend) else True:
-            quotient -= 1
+        step = -1
+
+    quotient = step
+
+    # a // b = c, if want to get c, we can use  b * c = a - t to get c and t.
+    while True if abs(quotient) * abs(divisor) < abs(dividend) else False:
+        quotient += step
+
+    # If step is 1 and remainder is not 0, means quotient is bigger one than needed; 
+    if 1 == step and quotient * divisor is not dividend:
+        quotient -= 1
 
     return quotient, dividend - quotient * divisor
 
 if __name__ == "__main__":
-    x, y = 8, 3
+    x = 8
     print ("mydivmod(%d, %d) = %r" % (0, 9, mydivmod(0, 9)))
-    print ("mydivmod(%d, %d) = %r" % (x, y, mydivmod(x, y)))
-    print ("mydivmod(%d, %d) = %r" % (-x, y, mydivmod(-x, y)))
-    print ("mydivmod(%d, %d) = %r" % (x, -y, mydivmod(x, -y)))
-    print ("mydivmod(%d, %d) = %r" % (-x, -y, mydivmod(-x, -y)))
+    print ("mydivmod(%d, %d) = %r" % (-5, 3, mydivmod(-5, 3)))
+    for y in range(1, 10):
+        print ("mydivmod(%d, %d) = %r" % (x, y, mydivmod(x, y)))
+        print ("mydivmod(%d, %d) = %r" % (-x, y, mydivmod(-x, y)))
+        print ("mydivmod(%d, %d) = %r" % (x, -y, mydivmod(x, -y)))
+        print ("mydivmod(%d, %d) = %r" % (-x, -y, mydivmod(-x, -y)))
