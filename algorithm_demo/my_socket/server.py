f34582083c22417608a531f2ffde0d7fc4312e09
linkcheng <zheng.long@shoufuyou.com>
2020-06-13 19:31:58 +0800

    selector server

--- algorithm_demo/my_socket/server.py
+++ algorithm_demo/my_socket/server.py
@@ -6,9 +6,10 @@
 @date: 2020-06-13 
 """
 import socket
+import selectors
 from socket import AF_INET, SOCK_STREAM
+from selectors import DefaultSelector, SelectorKey, EVENT_READ, EVENT_WRITE
 from typing import NewType, Union
-from handler import WriteHandler
 
 Socket = NewType('socket', socket)
 IP = '0.0.0.0'
@@ -38,17 +39,6 @@ class TcpServer1:
         self.clients[client_fd] = addr
         return client_fd
 
-    def recv(self, client_fd: Socket) -> str:
-        """接收数据"""
-        data = client_fd.recv(BUFFER_SIZE)
-        return data.decode('utf8')
-
-    def send(self, client_fd: Socket, data: Union[str, bytes]) -> None:
-        """发送数据"""
-        if isinstance(data, str):
-            data = data.encode('utf8')
-        client_fd.send(data)
-
     def close(self) -> None:
         """关闭"""
         for fd in self.clients:
@@ -58,20 +48,96 @@ class TcpServer1:
             self.server_fd.close()
 
 
+class Handler:
+    def __init__(self, server: TcpServer1):
+        self.server = server
+
+    def accept(self, sock) -> Socket:
+        """获取客户端 socket"""
+        if sock != self.server.server_fd:
+            return sock
+
+        client_fd, addr = self.server.server_fd.accept()
+        print(f'Client {addr} connected')
+        self.server.clients[client_fd] = addr
+        return client_fd
+
+    def read(self, client_fd: Socket):
+        """接收数据"""
+        data = client_fd.recv(BUFFER_SIZE)
+        return data.decode('utf8')
+
+    def send(self, client_fd: Socket, data: Union[str, bytes]) -> None:
+        """发送数据"""
+        if isinstance(data, str):
+            data = data.encode('utf8')
+        if data:
+            client_fd.send(data)
+
+    def handle(self, conn, msg):
+        print(f'Recv:{msg}, from {self.server.clients[conn]}')
+        return msg.replace("吗", "").replace("?", "!").replace("？", "!")
+
+    def close(self, conn):
+        conn.close()
+        addr = self.server.clients.pop(conn)
+        print(f'Client {addr} closed')
+
+
+class EventLoop:
+    def __init__(self, server_id, handler: Handler):
+        self.select = DefaultSelector()
+        self.server_id = server_id
+        self.handler = handler
+        self.buffer = {}
+
+    def register(self, fd: Socket, events: SelectorKey, data=None):
+        fd.setblocking(False)
+        self.select.register(fd, events, data)
+
+    def unregister(self, fd):
+        self.select.unregister(fd)
+
+    def run(self):
+        loop.register(self.server_id, selectors.EVENT_READ, self.accept)
+
+        while True:
+            events = self.select.select()
+            for key, mask in events:
+                if mask == EVENT_READ:
+                    if key.fileobj == self.server_id:
+                        self.accept(key.fileobj)
+                    else:
+                        self.read(key.fileobj)
+                elif mask == EVENT_WRITE:
+                    self.write(key.fileobj)
+
+    def accept(self, sock):
+        """sock 要处理的 fd, mask 为事件类型 READ or WRITE"""
+        conn = self.handler.accept(sock)  # Should be ready
+        self.register(conn, EVENT_READ | EVENT_WRITE, self.read)
+
+    def read(self, conn):
+        data = self.handler.read(conn)
+        if data and data not in ('q', 'Q'):
+            self.buffer[conn] = self.handler.handle(conn, data)
+        else:
+            self.unregister(conn)
+            self.handler.close(conn)
+
+    def write(self, conn):
+        self.handler.send(conn, self.buffer.pop(conn, None))
+
+
 if __name__ == '__main__':
-    writer_handler = WriteHandler()
-    server = TcpServer1(IP, PORT)
+    svr = TcpServer1(IP, PORT)
+    sfd = svr.create_server()
+    loop = EventLoop(sfd, Handler(svr))
 
-    sfd = server.create_server()
-    cfd = server.accept()
-
-    while True:
-        msg = server.recv(cfd)
-        if msg in ('q', 'Q'):
-            break
-
-        print(f'Recv:{msg}, from {server.clients[cfd]}')
-        server.send(cfd, writer_handler(msg))
-
-    server.close()
+    print('start server')
+    try:
+        loop.run()
+    finally:
+        print('close server')
+        svr.close()
 
